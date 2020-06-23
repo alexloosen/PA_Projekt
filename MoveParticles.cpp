3e56c453e510074c9739693bad37d735820aed9c
aloosen92 <alexander.loosen@gmx.de>
2020-06-23 13:52:03 +0200

    v5.4

--- MoveParticles.cpp
+++ MoveParticles.cpp
@@ -6,22 +6,20 @@ void MoveParticles(const int nr_Particles, Particle const partikel, const float 
 	// Abschwächung als zusätzlicher Abstand, um Singularität und Selbst-Interaktion zu vermeiden
 	const float softening = 1e-20f;
 	float Fx = .0f, Fy = .0f, Fz = .0f;
-	float dx = .0f, dy = .0f, dz = .0f;
+//	float dx = .0f, dy = .0f, dz = .0f;
 	// Schleife über alle Partikel
+#pragma ivdep
+//#pragma omp parallel
 	for (int i = 0; i < nr_Particles; i++) {
 		// Schleife über die anderen Partikel die Kraft auf Partikel i ausüben
 #pragma omp simd
 		for (int j = 0; j < nr_Particles; j++) {
-			__assume_aligned(partikel.x, 32);
-			__assume_aligned(partikel.y, 32);
-			__assume_aligned(partikel.z, 32);
 			// Gravitationsgesetz
 			// Berechne Abstand der Partikel i und j
-			dx = partikel.x[j] - partikel.x[i];
-			dy = partikel.y[j] - partikel.y[i];
-			dz = partikel.z[j] - partikel.z[i];
+			const float dx = partikel.x[j] - partikel.x[i];
+			const float dy = partikel.y[j] - partikel.y[i];
+			const float dz = partikel.z[j] - partikel.z[i];
 			const float drSquared = dx * dx + dy * dy + dz * dz + softening;
-
 			float drPower32 = sqrt(drSquared);
 			drPower32 = drPower32 * drPower32 * drPower32;
 
@@ -37,7 +35,6 @@ void MoveParticles(const int nr_Particles, Particle const partikel, const float 
 		Fy = .0f;
 		Fz = .0f;
 	}
-
 	// Bewege Partikel entsprechend der aktuellen Geschwindigkeit
 	for (int i = 0; i < nr_Particles; i++) {
 		partikel.x[i] += partikel.vx[i] * dt;
--- main.h
+++ main.h
@@ -11,12 +11,12 @@ struct Particle {
 	float *vx, *vy, *vz;  // Geschwindigkeiten des Teilchens
 	Particle(int nrParticles)
 	{
-		x = (float*)_mm_malloc(sizeof(float) * nrParticles, 32);
-		y = (float*)_mm_malloc(sizeof(float) * nrParticles, 32);
-		z = (float*)_mm_malloc(sizeof(float) * nrParticles, 32);
-		vx = (float*)_mm_malloc(sizeof(float) * nrParticles, 32);
-		vy = (float*)_mm_malloc(sizeof(float) * nrParticles, 32);
-		vz = (float*)_mm_malloc(sizeof(float) * nrParticles, 32);
+		x = (float*)_mm_malloc(sizeof(float) * nrParticles, (int)sizeof(float));
+		y = (float*)_mm_malloc(sizeof(float) * nrParticles, (int)sizeof(float));
+		z = (float*)_mm_malloc(sizeof(float) * nrParticles, (int)sizeof(float));
+		vx = (float*)_mm_malloc(sizeof(float) * nrParticles, (int)sizeof(float));
+		vy = (float*)_mm_malloc(sizeof(float) * nrParticles, (int)sizeof(float));
+		vz = (float*)_mm_malloc(sizeof(float) * nrParticles, (int)sizeof(float));
 	}
 };
 
