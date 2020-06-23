a4d5ccdc22ec27872f1269ee95007a5bedb7145d
aloosen92 <alexander.loosen@gmx.de>
2020-06-23 11:01:52 +0200

    Zwischenstand A9

--- MoveParticles.cpp
+++ MoveParticles.cpp
@@ -1,6 +1,7 @@
 #include "main.h"
 
-void MoveParticles(const int nr_Particles, Particle const partikel, const float dt) {
+void MoveParticles(const int nr_Particles, Particle const partikel, const float dt)
+{
 
 	// Abschwächung als zusätzlicher Abstand, um Singularität und Selbst-Interaktion zu vermeiden
 	const float softening = 1e-20f;
@@ -9,17 +10,20 @@ void MoveParticles(const int nr_Particles, Particle const partikel, const float 
 	float* arrZ = (float*)_mm_malloc(sizeof(float) * nr_Particles, 32);
 	float Fx = .0f, Fy = .0f, Fz = .0f;
 	const int max_num_threads = omp_get_max_threads();
+	float dx = .0f, dy = .0f, dz = .0f;
 	// Schleife über alle Partikel
-#pragma omp parallel num_threads(max_num_threads) for collapse(2) schedule(static)
-	for (int i = 0; i < nr_Particles; i++) {
 #pragma omp simd aligned(arrX:32, arrY:32, arrZ:32)
+	for (int i = 0; i < nr_Particles; i++) {
 		// Schleife über die anderen Partikel die Kraft auf Partikel i ausüben
+		__assume_aligned(partikel.x, 32);
+		__assume_aligned(partikel.y, 32);
+		__assume_aligned(partikel.z, 32);
 		for (int j = 0; j < nr_Particles; j++) {
 			// Gravitationsgesetz
 			// Berechne Abstand der Partikel i und j
-			const float dx = partikel.x[j] - partikel.x[i];
-			const float dy = partikel.y[j] - partikel.y[i];
-			const float dz = partikel.z[j] - partikel.z[i];
+			dx = partikel.x[j] - partikel.x[i];
+			dy = partikel.y[j] - partikel.y[i];
+			dz = partikel.z[j] - partikel.z[i];
 			const float drSquared = dx * dx + dy * dy + dz * dz + softening;
 
 			float drPower32 = sqrt(drSquared);
